a363f8d523cbad51b62e745b28057ce0b0a6ba95
ronald-kimeli <kimeliryans@gmail.com>
2023-10-04 17:44:31 +0300

    Liberator app

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\DashboardController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/profile', [DashboardController::class, 'profile'])->name('profile')->middleware('auth');
+
+Route::get('admin', function () {
+    return view('admin');
+})->name('admin')->middleware('admin');
+Route::get('hr', function () {
+    return view('hr');
+})->name('hr')->middleware('hr');
+Route::get('staff', function () {
+    return view('staff');
+})->name('staff')->middleware('staff');
+
+Route::get('/subscriptions', function () {
+    return view('inc.contents.subscriptions');
+});
+
+Route::get('/dashboard', function () {
+    return view('inc.contents.dashboard');
+});
+
+Route::get('/courses', function () {
+    return view('inc.contents.courses');
+});
+
+// Route::post('/data', function () {
+//     $resource = $_POST['resource'];
+//     if ($resource) {
+//         return view("inc.contents.$resource");
+//     } else {
+//         return view("inc.contents.dashboard");
+//     }
+// });
