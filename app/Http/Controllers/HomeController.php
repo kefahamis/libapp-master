c4617cb15592be33a6fae5d4d19f5fff449b15c5
kefahamis <hamiskf@gmail.com>
2023-10-05 11:40:46 +0300

    New changes

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Course;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $frontcourses = new Course;
+
+        dd($frontcourses);
+
+        // return view('welcome', compact(frontcourses));
+
+        // return redirect()->route('login');
+        
+        
+    }
+}
